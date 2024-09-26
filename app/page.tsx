7a7add021b2a3989ccfbc7b8af7682147d235b31
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-09-26 17:50:21 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="mx-auto max-w-xl py-12 md:py-24">
+      <div className="space-y-2 text-center ">
+        <h1 className="text-3xl md:text-3xl font-bold">URL Shortner</h1>
+        <p className="md:text-lg">Shorteen ur URl's and share them easily</p>
+      </div>
+    </main>
+  );
+}
